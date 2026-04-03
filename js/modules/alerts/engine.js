bf6566a441cdf57f48aeb21900a95ac2fbdb8311
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 21:01:40 +0530

    Update engine.js

--- js/modules/alerts/engine.js
+++ js/modules/alerts/engine.js
@@ -9,14 +9,20 @@ export function buildAlerts() {
     ads.forEach(r => {
 
         if (r.spend > 1000 && r.units === 0) {
-            alerts.push(`Spend wasted on campaign ${r.campaign_name}`);
+            alerts.push({
+                type: "danger",
+                msg: `High spend but no sales → ${r.campaign_name}`
+            });
         }
 
         if (r.revenue / r.spend < 1) {
-            alerts.push(`Low ROI campaign ${r.campaign_name}`);
+            alerts.push({
+                type: "warning",
+                msg: `Low ROI campaign → ${r.campaign_name}`
+            });
         }
 
     });
 
-    return alerts;
+    return alerts.slice(0, 50);
 }
