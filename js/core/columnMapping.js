7d69a47992618523636e6218202d23031b103263
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 16:53:55 +0530

    Update columnMapping.js

---
+++ js/core/columnMapping.js
@@ -0,0 +1,18 @@
+export const COLUMN_MAPPING = {
+
+    CDR: {
+        impressions: "impressions",
+        clicks: "clicks",
+        spend: "ad_spend",
+        revenue: "total_revenue",
+        units: "units_sold_total"
+    },
+
+    SALES: {
+        revenue: "final_amount",
+        units: "qty",
+        brand: "brand",
+        po_type: "po_type"
+    }
+
+};
