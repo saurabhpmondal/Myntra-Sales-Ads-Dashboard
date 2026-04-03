5319804c0b2534d802aa3cc67ec0ad7e33076292
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 22:14:58 +0530

    Update binder.js

--- js/modules/dashboard/binder.js
+++ js/modules/dashboard/binder.js
@@ -2,6 +2,18 @@ import { buildDashboard } from "./engine.js";
 import { renderDashboard } from "./ui.js";
 
 export function run() {
-    const data = buildDashboard();
-    renderDashboard(data);
+
+    try {
+
+        const data = buildDashboard();
+
+        renderDashboard(data);
+
+    } catch (e) {
+
+        console.error("Dashboard error:", e);
+
+        document.getElementById("content").innerHTML =
+            "<p>Error loading dashboard</p>";
+    }
 }
