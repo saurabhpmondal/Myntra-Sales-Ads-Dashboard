91825e1a7230d08dbf811365fa4db9a3068dff08
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 18:03:23 +0530

    Update stateManager.js

--- js/core/stateManager.js
+++ js/core/stateManager.js
@@ -1,5 +1,6 @@
 const state = {
-    data: {}
+    data: {},
+    filter: null
 };
 
 export function setData(name, data) {
@@ -8,4 +9,12 @@ export function setData(name, data) {
 
 export function getData(name) {
     return state.data[name] || [];
+}
+
+export function setFilter(filter) {
+    state.filter = filter;
+}
+
+export function getFilter() {
+    return state.filter;
 }
