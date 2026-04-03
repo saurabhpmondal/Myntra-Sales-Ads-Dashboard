4e0df3917340662d92b99dae7af6a4a19fe4e717
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 18:03:51 +0530

    Update engine.js

--- js/modules/dashboard/engine.js
+++ js/modules/dashboard/engine.js
@@ -1,27 +1,59 @@
-import { getData } from "../../core/stateManager.js";
+import { getData, getFilter } from "../../core/stateManager.js";
+import { applyDateFilter } from "../../filters/engine.js";
 
 export function buildDashboard() {
 
-    const sales = getData("SALES");
-    const ads = getData("CDR");
+    const filter = getFilter();
+
+    let sales = getData("SALES");
+    let ads = getData("CDR");
+
+    sales = applyDateFilter(sales, filter);
+    ads = applyDateFilter(ads, filter);
 
     let gmv = 0, units = 0;
+
+    const brandMap = {};
     const dailySales = {};
 
     sales.forEach(r => {
+
         gmv += r.revenue;
         units += r.units;
 
+        // brand aggregation
+        if (!brandMap[r.brand]) {
+            brandMap[r.brand] = {
+                gmv: 0,
+                units: 0,
+                PPMP: 0,
+                SJIT: 0,
+                SOR: 0
+            };
+        }
+
+        brandMap[r.brand].gmv += r.revenue;
+        brandMap[r.brand].units += r.units;
+
+        if (r.po_type) {
+            brandMap[r.brand][r.po_type] += r.revenue;
+        }
+
+        // daily
         if (!dailySales[r.date]) dailySales[r.date] = 0;
         dailySales[r.date] += r.revenue;
     });
 
-    let spend = 0, revenue = 0;
+    let spend = 0, revenue = 0, clicks = 0, impressions = 0;
+
     const dailyAds = {};
 
     ads.forEach(r => {
+
         spend += r.spend;
         revenue += r.revenue;
+        clicks += r.clicks;
+        impressions += r.impressions;
 
         if (!dailyAds[r.date]) dailyAds[r.date] = { spend: 0, revenue: 0 };
 
@@ -35,8 +67,11 @@ export function buildDashboard() {
             units,
             asp: units ? gmv / units : 0,
             spend,
-            revenue
+            revenue,
+            ctr: impressions ? clicks / impressions : 0,
+            roi: spend ? revenue / spend : 0
         },
+        brandMap,
         charts: {
             sales: dailySales,
             ads: dailyAds
