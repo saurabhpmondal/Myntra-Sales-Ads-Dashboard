edd128b7e681a7df09aa016b5c1bf829b75f2e90
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 16:53:24 +0530

    Update dataRegistry.js

---
+++ js/core/dataRegistry.js
@@ -0,0 +1,15 @@
+export const DATA_REGISTRY = {
+
+    CDR: {
+        url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1175680150&single=true&output=csv",
+        dateField: "date",
+        dateFormat: "YYYYMMDD"
+    },
+
+    SALES: {
+        url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1679615114&single=true&output=csv",
+        dateField: "date",
+        dateFormat: "DD-MM-YYYY"
+    }
+
+};
