b219cbab10041efee84c610a9215d15676e055d2
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 19:29:40 +0530

    Update appRouter.js

--- js/router/appRouter.js
+++ js/router/appRouter.js
@@ -1,4 +1,15 @@
 export async function loadModule(name) {
-    const module = await import(`../modules/${name}/binder.js`);
-    module.run();
+
+    try {
+        const module = await import(`../modules/${name}/binder.js`);
+
+        if (module.run) {
+            module.run();
+        } else {
+            console.error("run() not found in module:", name);
+        }
+
+    } catch (e) {
+        console.error("Module load error:", name, e);
+    }
 }
