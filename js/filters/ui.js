440cebce0aca5f4bc66c5b189c2496c6c79284b1
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 20:50:26 +0530

    Update ui.js

--- js/filters/ui.js
+++ js/filters/ui.js
@@ -5,10 +5,21 @@ export function renderFilters() {
     const el = document.getElementById("filters");
 
     el.innerHTML = `
-        <div class="card">
-            <button data-type="7d">Last 7 Days</button>
-            <button data-type="month">This Month</button>
-            <button data-type="lastMonth">Last Month</button>
+        <div class="card filter-bar">
+
+            <div class="filter-left">
+                <button data-type="7d">Last 7 Days</button>
+                <button data-type="month">This Month</button>
+                <button data-type="lastMonth">Last Month</button>
+            </div>
+
+            <div class="filter-right">
+                <input type="date" id="startDate">
+                <span>to</span>
+                <input type="date" id="endDate">
+                <button id="applyCustom">Apply</button>
+            </div>
+
         </div>
     `;
 
