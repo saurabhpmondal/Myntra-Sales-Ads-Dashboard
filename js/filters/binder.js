3d7035228a3a6ad4743641942e0a2d26ffdddd58
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 20:50:42 +0530

    Update binder.js

--- js/filters/binder.js
+++ js/filters/binder.js
@@ -4,16 +4,27 @@ import { loadModule } from "../router/appRouter.js";
 
 export function initFilters() {
 
+    // quick buttons
     document.querySelectorAll("[data-type]").forEach(btn => {
 
         btn.onclick = () => {
-
             const type = btn.dataset.type;
             const range = getFilterRange(type);
 
             setFilter(range);
-
             loadModule("dashboard");
         };
     });
+
+    // custom range
+    document.getElementById("applyCustom").onclick = () => {
+
+        const start = document.getElementById("startDate").value;
+        const end = document.getElementById("endDate").value;
+
+        if (!start || !end) return;
+
+        setFilter({ start, end });
+        loadModule("dashboard");
+    };
 }
