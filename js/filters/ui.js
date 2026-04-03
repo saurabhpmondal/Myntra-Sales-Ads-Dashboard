1fae1c2af949e35bd88a100c711a5ae9033b22a5
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 18:02:59 +0530

    Update ui.js

--- js/filters/ui.js
+++ js/filters/ui.js
@@ -1,3 +1,5 @@
+import { initFilters } from "./binder.js";
+
 export function renderFilters() {
 
     const el = document.getElementById("filters");
@@ -6,6 +8,9 @@ export function renderFilters() {
         <div class="card">
             <button data-type="7d">Last 7 Days</button>
             <button data-type="month">This Month</button>
+            <button data-type="lastMonth">Last Month</button>
         </div>
     `;
+
+    initFilters();
 }
