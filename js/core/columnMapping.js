345f4d9448efb4eea9bada0fa864ec4fff944dce
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 19:24:11 +0530

    Update columnMapping.js

--- js/core/columnMapping.js
+++ js/core/columnMapping.js
@@ -5,14 +5,33 @@ export const COLUMN_MAPPING = {
         clicks: "clicks",
         spend: "ad_spend",
         revenue: "total_revenue",
-        units: "units_sold_total"
+        units: "units_sold_total",
+        campaign_name: "campaign_name"
+    },
+
+    PPR: {
+        impressions: "impressions",
+        clicks: "clicks",
+        spend: "budget_spend",
+        revenue: "total_revenue",
+        units: "units_sold_total",
+        placement: "placement"
     },
 
     SALES: {
         revenue: "final_amount",
         units: "qty",
         brand: "brand",
-        po_type: "po_type"
+        po_type: "po_type",
+        style_id: "style_id"
+    },
+
+    TRAFFIC: {
+        impressions: "impressions",
+        clicks: "clicks",
+        add_to_carts: "add_to_carts",
+        purchases: "purchases",
+        style_id: "style_id"
     }
 
 };
