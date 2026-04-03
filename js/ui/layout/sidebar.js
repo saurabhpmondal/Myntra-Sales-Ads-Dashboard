4824f926ff7ff21356ae96c0b2b1c52c99a0159d
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-03 18:23:30 +0530

    Update sidebar.js

--- js/ui/layout/sidebar.js
+++ js/ui/layout/sidebar.js
@@ -3,15 +3,12 @@ export function renderSidebar() {
     const el = document.getElementById("sidebar");
 
     el.innerHTML = `
-        <h2 style="margin-bottom:20px;">Dashboard</h2>
+        <h2>Dashboard</h2>
 
-        <div class="nav">
-            <div data-module="dashboard">Dashboard</div>
-            <div data-module="campaign">Campaign</div>
-            <div data-module="product">Product</div>
-            <div data-module="placement">Placement</div>
-            <div data-module="listings">Listings</div>
-        </div>
+        <div data-module="dashboard">Dashboard</div>
+        <div data-module="campaign">Campaign</div>
+        <div data-module="placement">Placement</div>
+        <div data-module="product">Daily Ads</div>
     `;
 
     el.querySelectorAll("[data-module]").forEach(item => {
