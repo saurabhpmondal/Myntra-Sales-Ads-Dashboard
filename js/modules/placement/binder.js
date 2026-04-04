970964127b963c99beb34d6e5b3c9e5fd3a1fcc3
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-04 18:47:09 +0530

    Update binder.js

--- js/modules/placement/binder.js
+++ js/modules/placement/binder.js
@@ -1,7 +1,9 @@
-import { buildPlacementReport } from "./engine.js";
+import { buildPlacementData } from "./engine.js";
 import { renderPlacement } from "./ui.js";
 
-export function run() {
-    const data = buildPlacementReport();
+export function runPlacement(){
+
+    const data = buildPlacementData();
+
     renderPlacement(data);
 }
