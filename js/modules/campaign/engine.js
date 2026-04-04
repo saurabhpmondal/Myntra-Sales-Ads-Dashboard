3a5dd1a16ad57126158431de0d845bbdb52d723e
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-04 16:18:05 +0530

    Update engine.js

--- js/modules/campaign/engine.js
+++ js/modules/campaign/engine.js
@@ -1,22 +1,38 @@
-import { applyGlobalFilters } from "../../core/filterEngine.js";
 import { getData } from "../../core/dataRegistry.js";
 
-export function buildDailyAdsData() {
+export function buildCampaignData(){
 
-    const ads = applyGlobalFilters(getData("CDR"));
+    const raw = getData("CDR") || [];
+
+    const state = window.APP_STATE || {};
+    const from = state.from;
+    const to = state.to;
+    const brand = state.brand;
+
+    const data = raw.filter(r => {
+
+        const d = r.date;
+        if (!d) return false;
+
+        if (from && d < from) return false;
+        if (to && d > to) return false;
+
+        if (brand && r.brand && r.brand !== brand) return false;
+
+        return true;
+    });
 
     const map = {};
 
-    ads.forEach(r => {
+    data.forEach(r => {
 
-        const d = r.date;
-        if (!d) return;
+        const key = r.campaign_name || "Unknown";
 
-        if (!map[d]) {
-            map[d] = {
-                spend: 0,
+        if (!map[key]) {
+            map[key] = {
                 impressions: 0,
                 clicks: 0,
+                spend: 0,
 
                 direct_units: 0,
                 indirect_units: 0,
@@ -26,30 +42,26 @@ export function buildDailyAdsData() {
             };
         }
 
-        map[d].spend += r.ad_spend || 0;
-        map[d].impressions += r.impressions || 0;
-        map[d].clicks += r.clicks || 0;
+        map[key].impressions += Number(r.impressions || 0);
+        map[key].clicks += Number(r.clicks || 0);
+        map[key].spend += Number(r.ad_spend || 0);
 
-        map[d].direct_units += r.direct_units_sold || 0;
-        map[d].indirect_units += r.indirect_units_sold || 0;
+        map[key].direct_units += Number(r.direct_units_sold || 0);
+        map[key].indirect_units += Number(r.indirect_units_sold || 0);
 
-        map[d].direct_rev += r.direct_revenue || 0;
-        map[d].indirect_rev += r.indirect_revenue || 0;
+        map[key].direct_rev += Number(r.direct_revenue || 0);
+        map[key].indirect_rev += Number(r.indirect_revenue || 0);
     });
 
-    // DERIVED METRICS
     Object.values(map).forEach(r => {
 
-        r.total_units = r.direct_units + r.indirect_units;
-        r.total_rev = r.direct_rev + r.indirect_rev;
+        r.units = r.direct_units + r.indirect_units;
+        r.revenue = r.direct_rev + r.indirect_rev;
 
         r.ctr = r.impressions ? r.clicks / r.impressions : 0;
-        r.cvr = r.clicks ? r.total_units / r.clicks : 0;
-        r.cpc = r.clicks ? r.spend / r.clicks : 0;
+        r.cvr = r.clicks ? r.units / r.clicks : 0;
 
-        r.roi_direct = r.spend ? r.direct_rev / r.spend : 0;
-        r.roi_indirect = r.spend ? r.indirect_rev / r.spend : 0;
-        r.roi_total = r.spend ? r.total_rev / r.spend : 0;
+        r.roi = r.spend ? r.revenue / r.spend : 0;
     });
 
     return map;
