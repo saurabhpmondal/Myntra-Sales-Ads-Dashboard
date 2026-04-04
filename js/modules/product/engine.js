48fd29992bdc4919990b7423b1094ea188d5ac4f
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-04 15:10:31 +0530

    Update engine.js

--- js/modules/product/engine.js
+++ js/modules/product/engine.js
@@ -1,31 +1,55 @@
-import { getData } from "../../core/stateManager.js";
-import { getFilter } from "../../core/stateManager.js";
-import { applyDateFilter } from "../../filters/engine.js";
+import { applyGlobalFilters } from "../../core/filterEngine.js";
+import { getData } from "../../core/dataRegistry.js";
 
-export function buildDailyAds() {
+export function buildDailyAdsData() {
 
-    let data = getData("CDR");
-    const filter = getFilter();
-
-    data = applyDateFilter(data, filter);
+    const ads = applyGlobalFilters(getData("CDR"));
 
     const map = {};
 
-    data.forEach(r => {
+    ads.forEach(r => {
 
-        if (!map[r.date]) {
-            map[r.date] = {
+        const d = r.date;
+        if (!d) return;
+
+        if (!map[d]) {
+            map[d] = {
                 spend: 0,
-                revenue: 0,
+                impressions: 0,
                 clicks: 0,
-                impressions: 0
+
+                direct_units: 0,
+                indirect_units: 0,
+
+                direct_rev: 0,
+                indirect_rev: 0
             };
         }
 
-        map[r.date].spend += r.spend;
-        map[r.date].revenue += r.revenue;
-        map[r.date].clicks += r.clicks;
-        map[r.date].impressions += r.impressions;
+        map[d].spend += r.ad_spend || 0;
+        map[d].impressions += r.impressions || 0;
+        map[d].clicks += r.clicks || 0;
+
+        map[d].direct_units += r.direct_units_sold || 0;
+        map[d].indirect_units += r.indirect_units_sold || 0;
+
+        map[d].direct_rev += r.direct_revenue || 0;
+        map[d].indirect_rev += r.indirect_revenue || 0;
+    });
+
+    // DERIVED METRICS
+    Object.values(map).forEach(r => {
+
+        r.total_units = r.direct_units + r.indirect_units;
+        r.total_rev = r.direct_rev + r.indirect_rev;
+
+        r.ctr = r.impressions ? r.clicks / r.impressions : 0;
+        r.cvr = r.clicks ? r.total_units / r.clicks : 0;
+        r.cpc = r.clicks ? r.spend / r.clicks : 0;
+
+        r.roi_direct = r.spend ? r.direct_rev / r.spend : 0;
+        r.roi_indirect = r.spend ? r.indirect_rev / r.spend : 0;
+        r.roi_total = r.spend ? r.total_rev / r.spend : 0;
     });
 
     return map;
