d9ff827728675ee4c382603fbf3334662c41a53a
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-04 09:03:12 +0530

    Update binder.js

--- js/filters/binder.js
+++ js/filters/binder.js
@@ -1,5 +1,25 @@
 import { renderFilters } from "./ui.js";
+import { runDashboard } from "../modules/dashboard/binder.js";
 
 export function initFilters() {
+
     renderFilters();
+
+    document.getElementById("applyFilters")
+        .addEventListener("click", applyFilters);
+}
+
+function applyFilters() {
+
+    const from = document.getElementById("fromDate").value;
+    const to = document.getElementById("toDate").value;
+    const brand = document.getElementById("brandFilter").value;
+
+    window.APP_STATE = {
+        from,
+        to,
+        brand
+    };
+
+    runDashboard();
 }
