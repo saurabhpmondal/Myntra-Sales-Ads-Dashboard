60058a161559a1846b9945c455ddd0eace2af844
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-04 08:45:23 +0530

    Update ui.js

--- js/filters/ui.js
+++ js/filters/ui.js
@@ -1,27 +1,12 @@
-import { initFilters } from "./binder.js";
-
 export function renderFilters() {
 
     const el = document.getElementById("filters");
 
     el.innerHTML = `
-        <div class="card filter-bar">
-
-            <div class="filter-left">
-                <button data-type="7d">Last 7 Days</button>
-                <button data-type="month">This Month</button>
-                <button data-type="lastMonth">Last Month</button>
-            </div>
-
-            <div class="filter-right">
-                <input type="date" id="startDate">
-                <span>to</span>
-                <input type="date" id="endDate">
-                <button id="applyCustom">Apply</button>
-            </div>
-
+        <div class="filter-bar">
+            <button data-type="7d">Last 7 Days</button>
+            <button data-type="month">This Month</button>
+            <button data-type="lastMonth">Last Month</button>
         </div>
     `;
-
-    initFilters();
 }
