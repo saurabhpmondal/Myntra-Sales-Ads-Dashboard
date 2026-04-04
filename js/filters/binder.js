4a7691f1e5c37a37802aba0a464561895ff5475a
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-04 08:45:40 +0530

    Update binder.js

--- js/filters/binder.js
+++ js/filters/binder.js
@@ -1,30 +1,5 @@
-import { getFilterRange } from "./engine.js";
-import { setFilter } from "../core/stateManager.js";
-import { loadModule } from "../router/appRouter.js";
+import { renderFilters } from "./ui.js";
 
 export function initFilters() {
-
-    // quick buttons
-    document.querySelectorAll("[data-type]").forEach(btn => {
-
-        btn.onclick = () => {
-            const type = btn.dataset.type;
-            const range = getFilterRange(type);
-
-            setFilter(range);
-            loadModule("dashboard");
-        };
-    });
-
-    // custom range
-    document.getElementById("applyCustom").onclick = () => {
-
-        const start = document.getElementById("startDate").value;
-        const end = document.getElementById("endDate").value;
-
-        if (!start || !end) return;
-
-        setFilter({ start, end });
-        loadModule("dashboard");
-    };
+    renderFilters();
 }
